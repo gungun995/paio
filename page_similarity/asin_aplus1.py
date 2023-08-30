f3fb5d1b0d9003e03c922fcd79cdfae2cd701703
gungun995 <1164437787@qq.com>
2023-08-30 21:41:50 +0800

    把源表更新为了page_product_info，是八月新爬的美国站数据但和原来大差不差

---
+++ page_similarity/asin_aplus1.py
@@ -0,0 +1,88 @@
+import jellyfish
+import numpy as np
+from database1 import *
+from collections import Counter
+
+# 字符串与编码的字典
+string_encoding_dict = {}
+a = []
+with db1.cursor() as cursor:
+    cursor.execute("SELECT * FROM ods_amazon_page_product_info")
+    for row in cursor:
+        log1 = row[0]
+        a.append(log1)
+        string_encoding_dict[row[0]] = row
+
+
+class asin_aplus(Model):
+    ASIN = CharField()
+    Aplus = CharField()
+    recommend_Aplus = CharField()
+
+    class Meta:
+        database = db2
+        db_table = "dwd_asin_aplus"
+
+
+asin_aplus.create_table()
+
+# 计算相似度矩阵
+similarity_matrix = []
+for asin1, page1 in string_encoding_dict.items():
+    row = []
+    for asin2, page2 in string_encoding_dict.items():
+        # 计算Jaccard相似度
+        if asin1 != asin2:
+            if page1[8] and page2[8]:
+                jac_sim = jellyfish.jaro_similarity(page1[8], page2[8])
+                row.append(jac_sim)
+            else:
+                jac_sim = 0
+                row.append(jac_sim)
+        else:
+            jac_sim = 0
+            row.append(jac_sim)
+    similarity_matrix.append(row)
+
+for asin in a:
+    # 将原表中的aplus数据格式化为列表
+    # aplus = page_info.select(page_info.aplus)
+    self_aplus = Page.select().where(Page.product_asin == asin).get()
+    # print(eval(self_aplus.aplus))
+
+    # 输入字符串索引
+    input_idx = list(string_encoding_dict.keys()).index(asin)
+
+    # 排序相似度矩阵对应行,取最后5个索引
+    top_5_idx = np.argsort(similarity_matrix[input_idx])[-5:]
+
+    # 获取top 5字符串
+    top_5 = [string_encoding_dict[a[i]] for i in top_5_idx[::-1]]  # i为索引
+
+    b = []
+    for row in top_5:
+        aplus = row[11]
+        # aplus = aplus.replace("\\", "")  # 这里使用了\\转义为\
+        aplus = eval(aplus)
+        for i in aplus:
+            b.append(i)
+    # 修改过了 之前在循环内部 虽然最终效果一样，但重复了无效计算
+    count = Counter(b)
+    most_common = count.most_common(5)
+    most_common = [i[0] for i in most_common]
+    # 求出自身A+标签
+    self_aplus = eval(self_aplus.aplus)
+    # 页面已有的A+标签不用在推荐列表里面再显示一次
+    most_common1 = []
+    for i in most_common:
+        if i not in self_aplus:
+            most_common1.append(i)
+    # 以下为错误操作
+    # for i in most_common:
+    #     if i in self_aplus:
+    #         most_common.remove(i)
+    # 存表
+    object = asin_aplus(
+        ASIN=asin, Aplus=str(self_aplus), recommend_Aplus=str(most_common1)
+    )
+    object.save()
--- page_similarity/database1.py
+++ page_similarity/database1.py
@@ -1,29 +1,75 @@
 from peewee import *
-db1 = MySQLDatabase('paio-ods', host='210.45.215.210', port=6400, user='paio', password='C@$=[2023]&t!(p@!0)')
-db2 = MySQLDatabase('paio-stage', host='210.45.215.210', port=6400, user='paio', password='C@$=[2023]&t!(p@!0)')
+
+db1 = MySQLDatabase(
+    "paio-ods",
+    host="210.45.215.210",
+    port=6400,
+    user="paio",
+    password="C@$=[2023]&t!(p@!0)",
+)
+db2 = MySQLDatabase(
+    "paio-stage",
+    host="210.45.215.210",
+    port=6400,
+    user="paio",
+    password="C@$=[2023]&t!(p@!0)",
+)
+
 
 class page_info(Model):
-   id=AutoField()
-   # id=IntegerField(primary_key=True)
-   product_asin=CharField()
-   title=CharField()
-   base_info=CharField()
-   feature = CharField()
-   price = CharField()
-   technical_details=CharField()
-   addition_details=CharField()
-   rank=CharField()
-   reviews_star = CharField()
-   fearure_scores = CharField()
-   aplus = CharField()
-   all_reviews = CharField()
-   positive_reviews = TextField()
-   critical_reviews = TextField()
-   product_problems = TextField()
-   comptition_asin = CharField()
-   sponsored_asin = CharField()
-   date = DateField()
-   version=CharField()
-   class Meta:
-      database=db1
-      db_table='ods_amazon_page_page_info'
+    id = AutoField()
+    # id=IntegerField(primary_key=True)
+    product_asin = CharField()
+    title = CharField()
+    base_info = CharField()
+    feature = CharField()
+    price = CharField()
+    technical_details = CharField()
+    addition_details = CharField()
+    rank = CharField()
+    reviews_star = CharField()
+    fearure_scores = CharField()
+    aplus = CharField()
+    all_reviews = CharField()
+    positive_reviews = TextField()
+    critical_reviews = TextField()
+    product_problems = TextField()
+    comptition_asin = CharField()
+    sponsored_asin = CharField()
+    date = DateField()
+    version = CharField()
+
+    class Meta:
+        database = db1
+        db_table = "ods_amazon_page_page_info"
+
+
+class Page(Model):
+    """id=IntegerField(primary_key=True)'"""
+
+    product_asin = CharField(collation="utf8mb4_unicode_ci")
+    country_code = CharField(collation="utf8mb4_unicode_ci")
+    title = CharField(collation="utf8mb4_unicode_ci")
+    base_info = TextField()
+    feature = TextField()
+    price = CharField(collation="utf8mb4_unicode_ci")
+    technical_details = TextField()
+    addition_details = TextField()
+    rank = CharField(collation="utf8mb4_unicode_ci")
+    reviews_star = CharField(collation="utf8mb4_unicode_ci")
+    fearure_scores = CharField(collation="utf8mb4_unicode_ci")
+    aplus = TextField()
+    all_reviews = TextField()
+    positive_reviews = TextField()
+    critical_reviews = TextField()
+    product_problems = TextField()
+    comptition_asin = TextField()
+    sponsored_asin = TextField()
+    date = DateField()
+    version = CharField()
+
+    class Meta:
+        database = db1
+        db_table = "ods_amazon_page_product_info"
+        primary_key = CompositeKey("product_asin", "country_code", "date")
+        collation = "utf8mb4_unicode_ci"
